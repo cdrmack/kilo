1bfadd367887b19868744619bd612206f77de57f
cdrmack <git.pynvt@passmail.net>
2023-11-22 16:21:48 +0100

    restore original term attributes on exit

--- kilo.c
+++ kilo.c
@@ -1,12 +1,24 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios original_termios;
+
+// there are two general kinds of input processing:
+// canonical (cooked) and noncanonical (raw)
+void disable_raw_mode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
+}
+
 void enable_raw_mode()
 {
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO); // bitwise-NOT and bitwise-AND
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    tcgetattr(STDIN_FILENO, &original_termios);
+    atexit(disable_raw_mode);
+
+    struct termios raw_termios = original_termios;
+    raw_termios.c_lflag &= ~(ECHO); // bitwise-NOT and bitwise-AND
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_termios);
 }
 
 int main()
