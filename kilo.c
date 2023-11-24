59a8b2cece860a932ee6a935d8448d15bb20a752
cdrmack <git.pynvt@passmail.net>
2023-11-24 06:47:40 +0100

    disable OPOST (post-processing of output)
    by default terminal translates each new line '\n' into
    carriage return `\r` (move cursor back to the beginning of the current
    line) and the new line `\n` that moves cursor down a line

--- kilo.c
+++ kilo.c
@@ -23,8 +23,9 @@ void enable_raw_mode(void)
     atexit(disable_raw_mode);
 
     struct termios raw_termios = original_termios;
-    raw_termios.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // bitwise-NOT and bitwise-AND
     raw_termios.c_iflag &= ~(ICRNL | IXON);
+    raw_termios.c_oflag &= ~(OPOST);
+    raw_termios.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // bitwise-NOT and bitwise-AND
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_termios);
 }
 
