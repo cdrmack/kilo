b589c8248d1169de1bc8756b4e7b86fdb8b849a0
cdrmack <git.pynvt@passmail.net>
2024-01-04 19:04:42 +0100

    add PAGE_UP and PAGE_DOWN

--- kilo.c
+++ kilo.c
@@ -17,7 +17,9 @@ enum editor_key
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+    PAGE_UP,
+    PAGE_DOWN
 };
 
 /*** DATA ***/
@@ -100,7 +102,7 @@ editor_read_key(void)
         return c;
     }
 
-    char seq[2];
+    char seq[3];
 
     if (read(STDIN_FILENO, &seq[0], 1) != 1)
     {
@@ -114,18 +116,39 @@ editor_read_key(void)
 
     if (seq[0] == '[')
     {
-        switch (seq[1])
+        if (seq[1] >= '0' && seq[1] <= '9')
         {
-            case 'A':
-                return ARROW_UP;
-            case 'B':
-                return ARROW_DOWN;
-            case 'C':
-                return ARROW_RIGHT;
-            case 'D':
-                return ARROW_LEFT;
-            default:
+            if (read(STDIN_FILENO, &seq[2], 1) != 1)
+            {
                 return '\x1b';
+            }
+
+            if (seq[2] == '~')
+            {
+                switch (seq[1])
+                {
+                    case '5':
+                        return PAGE_UP;
+                    case '6':
+                        return PAGE_DOWN;
+                }
+            }
+        }
+        else
+        {
+            switch (seq[1])
+            {
+                case 'A':
+                    return ARROW_UP;
+                case 'B':
+                    return ARROW_DOWN;
+                case 'C':
+                    return ARROW_RIGHT;
+                case 'D':
+                    return ARROW_LEFT;
+                default:
+                    return '\x1b';
+            }
         }
     }
     return '\x1b';
@@ -313,6 +336,16 @@ editor_process_keypress(void)
         case ARROW_RIGHT:
             editor_move_cursor(key);
             break;
+        case PAGE_UP:
+        case PAGE_DOWN:
+        {
+            int times = EDITOR_CONF.screenrows;
+            while (times--)
+            {
+                editor_move_cursor(key == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+            }
+            break;
+        }
         default:
             break;
     }
